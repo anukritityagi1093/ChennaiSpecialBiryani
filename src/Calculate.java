b4de3fa1d2eb54b762c055f7019446c741a498bc
Anukriti Tyagi <anukritityagi@MacBook-Air.local>
2019-07-29 13:13:45 +0530

    Initial Commit

---
+++ src/Calculate.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Calculate {
+	
+	public static int add(int x, int y) {
+		return x+y;
+	}
+	
+	public static int sub(int x, int y) {
+		return x-y;
+	}
+	
+	public static int mul(int x, int y) {
+		return x*y;
+	}
+	
+	public static int div(int x, int y) {
+		return x/y;
+		
+	}
+	
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int x= sc.nextInt();
+		int y= sc.nextInt();
+		
+		System.out.println(add(x,y));
+		System.out.println(sub(x,y));
+		System.out.println(mul(x,y));
+		System.out.println(div(x,y));
+		
+	}
+}
